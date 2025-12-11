3b432c639bf8db887cd9da3ad5ce7114dd155d9c
KevDev8 <71610282+KevDev8@users.noreply.github.com>
2025-12-11 03:41:14 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="container py-4"> <!-- Organizes the home page as a container with some vertical padding -->
+    <component :is="loggedIn ? 'DataGrid' : 'LoginPage'" 
+                @login-success="handleLoginSuccess"
+                :password="password"
+    /> <!-- Checks if loggedIn is true or false and loads DataGrid or LoginPage accordingly, passes password as a prop for either of them,
+            and listens for a login-sucess event from LoginPage to switch loggedIn to true through handleLoginSuccess -->
+  </div>
+</template>
+
+<script>
+import LoginPage from './components/LoginPage.vue'
+import DataGrid from './components/DataGrid.vue'
+
+export default {
+  name: 'App',
+  components: { LoginPage, DataGrid },
+  data() {
+    return {
+      loggedIn: false,
+      password: 'cardinals',
+    }
+  },
+  methods: { handleLoginSuccess() { this.loggedIn = true } }
+}
+</script>
